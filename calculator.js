ad08e11d056a037095c60c85a1e61dcef54710ee
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-02-03 19:15:50 -0800

    created a calculator program on 02-03-23

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.
+
+let firstNumber = prompt();
+let secondNumber = prompt();
+
+function addCalculator() {
+  let sum = firstNumber + secondNumber;
+  return sum;
+}
+
+// BONUS: Make a program that can subtract, multiply, and also divide!
